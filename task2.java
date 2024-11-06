88e0aa630db9f4b40d0817541b4aa4dffb646c2e
edgarl0l <98063734+edgarl0l@users.noreply.github.com>
2024-11-06 12:23:02 +0300

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,26 @@
+package mod;
+import java.util.HashMap;
+
+public class task2 {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numToIndex = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                return new int[] { numToIndex.get(complement), i };
+            }
+            numToIndex.put(nums[i], i);
+        }
+        
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+}
